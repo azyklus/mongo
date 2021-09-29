c2e6dcb0075b834dfd2a3f53f8695c491bbbfbb7
Smooth Operator <github@andy.disruptek.com>
2021-09-29 16:24:40 -0400

    cleaning up some networking

--- mongo/threaded.nim
+++ mongo/threaded.nim
@@ -20,11 +20,20 @@ type
   SharedChannel[T] = ptr Channel[T]
   SharedLock = ptr Lock
 
+  Reader = SharedChannel[(int64, seq[Bson])]
+  Writer = SharedChannel[string]
+  Context = object
+    reader: Reader
+    writer: Writer
+    host: string
+    port: Port
+
   Mongo* = ref object of MongoBase    ## Mongo client object
     requestLock:                    SharedLock
     pool {.guard: requestLock.}:    seq[LockedSocket]
-    threads:                        seq[Thread[(SharedChannel[(int64, seq[Bson])], SharedChannel[string], string, Port)]]
-    current:                        int ## Current (possibly) free socket to use
+    threads:                        seq[Thread[Context]]
+    current:                        int ##
+    ## Current (possibly) free socket to use
 
   LockedSocket* = ref object of LockedSocketBase
     id:         int
@@ -65,7 +74,8 @@ proc newLockedSocket(): LockedSocket =
   result.reader = newSharedChannel[(int64, seq[Bson])]()
   result.writer = newSharedChannel[string]()
 
-proc handleResponses(args: (SharedChannel[(int64, seq[Bson])], SharedChannel[string], string, Port)) {.thread.}
+proc handleResponses(context: Context) {.thread.}
+
 proc initPool(m: var Mongo, maxConnections: int) =
   m.threads = @[]
   m.threads.setLen(maxConnections)
@@ -75,10 +85,15 @@ proc initPool(m: var Mongo, maxConnections: int) =
     for i in 0..<maxConnections:
       m.pool[i] = newLockedSocket()
       m.pool[i].id = i
-      createThread(m.threads[i], handleResponses, (m.pool[i].reader, m.pool[i].writer, m.host, Port(m.port)))
+      let context = Context(reader: m.pool[i].reader,
+                            writer: m.pool[i].writer,
+                            host: m.host,
+                            port: Port m.port)
+      createThread(m.threads[i], handleResponses, context)
       m.pool[i].connected = true
 
-proc newMongo*(host: string = "127.0.0.1", port: uint16 = DefaultMongoPort, secure=false, maxConnections=16): Mongo =
+proc newMongo*(host = "127.0.0.1"; port = DefaultMongoPort;
+               secure=false, maxConnections=16): Mongo =
   ## Mongo client constructor
   result.new()
   result.init(host, port)
@@ -91,9 +106,12 @@ proc newMongoWithURI*(u: Uri, maxConnections=16): Mongo =
   result.initPool(maxConnections)
   result.current = -1
 
-proc newMongoWithURI*(u: string, maxConnections=16): Mongo = newMongoWithURI(parseUri(u), maxConnections)
+proc newMongoWithURI*(u: string, maxConnections=16): Mongo =
+  newMongoWithURI(parseUri(u), maxConnections)
 
-proc authenticateScramSha1(db: Database[Mongo], username: string, password: string, ls: LockedSocket): bool {.discardable, gcsafe.}
+proc authenticateScramSha1(db: Database[Mongo]; username, password: string;
+                           ls: LockedSocket): bool {.discardable, gcsafe.}
+
 proc acquire*(m: Mongo): LockedSocket =
   ## Retrieves next non-in-use socket for request
   while true:
@@ -116,7 +134,9 @@ proc release*(m: Mongo, ls: LockedSocket) =
     else:
       raise newException(ValueError, "Socket can't be released twice")
 
-method kind*(sm: Mongo): ClientKind = ClientKindSync       ## Sync Mongo client
+method kind*(sm: Mongo): ClientKind =
+  ## Sync Mongo client
+  ClientKindSync
 
 proc connect*(m: Mongo): bool =
   ## Establish connection with Mongo server
@@ -131,56 +151,82 @@ proc newMongoDatabase*(u: Uri): Database[Mongo] {.deprecated.} =
     result = m[u.path.extractFilename()]
     withRequestLock:
       for s in m.pool:
-        s.authenticated = result.authenticateScramSha1(m.username, m.password, s)
+        s.authenticated =
+          result.authenticateScramSha1(m.username, m.password, s)
 
-proc handleResponses(args: (SharedChannel[(int64, seq[Bson])], SharedChannel[string], string, Port)) {.thread.}=
-  let (reader, writer, host, port) = args
+
+proc consumeReply(reader: Reader; data: string) =
+  ## consume a reply and send cursorId and bson documents to reader
+  var stream = newStringStream(data)
+  discard stream.readInt32()                     ## requestId
+  discard stream.readInt32()                     ## responseTo
+  discard stream.readInt32()                     ## opCode
+  let responseFlags = stream.readInt32()         ## responseFlags
+  var cursorId = stream.readInt64()              ## cursorID
+  discard stream.readInt32()                     ## startingFrom
+  let numberReturned = stream.readInt32()        ## numberReturned
+  var res: seq[Bson]
+  if (responseFlags and RFCursorNotFound) != 0:
+    cursorId = 0
+  if numberReturned > 0:
+    for i in 0..<numberReturned:
+      res.add newBsonDocument(stream)
+  reader[].send((cursorId, res))
+
+proc readExactly(sock: Socket; buffer: var string; want: Natural) =
+  ## a read that's tolerant of incomplete recv() from the os
+  var size = want
+  var existing = buffer.len
+  var data = newStringOfCap size
+  while size > 0:
+    let read = sock.recv(data, size)
+    if read == 0:
+      raise CommunicationError.newException:
+        "server disconnected during read of $# bytes" % [ $size ]
+    else:
+      buffer.add data
+      if read <= size:
+        size -= read
+
+  if want != buffer.len-existing:
+    raise Defect.newException:
+      "wanted $# but received only $#" %
+        [ $want, $(buffer.len - existing) ]
+
+proc handleResponses(context: Context) {.thread.} =
+  let (reader, writer, host, port) =
+    (context.reader, context.writer, context.host, context.port)
   let sock = newSocket()
-  var needRaise = false
   sock.connect(host, port)
+  var data: string  # prevent continuous reallocation of string buffers
   while true:
     let pcktToSend = writer[].recv()
     if pcktToSend == "":
       break
-    if sock.trySend(pcktToSend):
-      var data: string = newStringOfCap(4)
-      var received: int = sock.recv(data, 4)
-      if received == 0:
-        needRaise = true
-        break
-      var stream: Stream = newStringStream(data)
+    if not sock.trySend(pcktToSend):
+      raise CommunicationError.newException:
+        "unable to send packet to MongoDB server"
 
-      ## Read data
-      let messageLength: int32 = stream.readInt32() - 4
-      data = newStringOfCap(messageLength)
-      received = sock.recv(data, messageLength)
-      if received == 0:
-        needRaise = true
-        break
-      stream = newStringStream(data)
+    # i guess we're just reading the reply length here
+    data.setLen 0
+    sock.readExactly(data, sizeof int32)
 
-      discard stream.readInt32()                     ## requestId
-      discard stream.readInt32()                     ## responseTo
-      discard stream.readInt32()                     ## opCode
-      let responceFlags = stream.readInt32()         ## responseFlags
-      var cursorId = stream.readInt64()              ## cursorID
-      discard stream.readInt32()                     ## startingFrom
-      let numberReturned: int32 = stream.readInt32() ## numberReturned
-      var res: seq[Bson] = @[]
+    # using stream's readInt32 to unpack an int32 🙄
+    var stream = newStringStream data
+    let messageLength = stream.readInt32() - sizeof(int32)
+    data.setLen 0
 
-      if (responceFlags and RFCursorNotFound) != 0:
-        cursorId = 0
-      if numberReturned > 0:
-        for i in 0..<numberReturned:
-          res.add(newBsonDocument(stream))
-      reader[].send((cursorId, res))
-  if needRaise:
-    raise newException(CommunicationError, "Disconnected from MongoDB server")
+    # now read the message body itself
+    sock.readExactly(data, messageLength)
+
+    # deliver the response to the reader
+    consumeReply(reader, data)
 
 proc refresh*(f: Cursor[Mongo], lockedSocket: LockedSocket = nil): seq[Bson] =
   ## Private procedure for performing actual query to Mongo
   if f.isClosed():
-    raise newException(CommunicationError, "Cursor can't be closed while requesting")
+    raise CommunicationError.newException:
+      "Cursor can't be closed while requesting"
 
   var res: string
   let numberToReturn = calcReturnSize(f)
@@ -209,9 +255,10 @@ proc refresh*(f: Cursor[Mongo], lockedSocket: LockedSocket = nil): seq[Bson] =
     for doc in data:
       if doc.contains("$err"):
         if doc["code"].toInt == 50:
-          raise newException(OperationTimeout, "Command " & $f & " has timed out")
+          raise OperationTimeout.newException:
+            "Command " & $f & " has timed out"
   elif data.len == 0 and numberToReturn == 1:
-    raise newException(NotFound, "No documents matching query were found")
+    raise NotFound.newException "No documents matching query were found"
   else:
     discard
   return data
@@ -220,10 +267,11 @@ proc one(f: Cursor[Mongo], ls: LockedSocket): Bson =
   # Internal proc used for sending authentication requests on particular socket
   let docs = f.limit(1).refresh(ls)
   if docs.len == 0:
-    raise newException(NotFound, "No documents matching query were found")
+    raise NotFound.newException "No documents matching query were found"
   return docs[0]
 
-proc authenticateScramSha1(db: Database[Mongo], username: string, password: string, ls: LockedSocket): bool {.discardable, gcsafe.} =
+proc authenticateScramSha1(db: Database[Mongo]; username, password: string;
+                           ls: LockedSocket): bool {.discardable, gcsafe.} =
   ## Authenticate connection (sync): using SCRAM-SHA-1 auth method
   if username == "" or password == "":
     return false
@@ -238,36 +286,40 @@ proc authenticateScramSha1(db: Database[Mongo], username: string, password: stri
     "autoAuthorize": 1'i32
   }
   let responseStart = db["$cmd"].makeQuery(requestStart).one(ls)
+
   ## line to check if connect worked
-  if isNil(responseStart) or not isNil(responseStart["code"]): return false #connect failed or auth failure
+  if responseStart.isNil or not responseStart["code"].isNil:
+    return false #connect failed or auth failure
   db.client.authenticated = true
   let
     responsePayload = binstr(responseStart["payload"])
     passwordDigest = $toMd5("$#:mongo:$#" % [username, password])
-    clientFinalMessage = scramClient.prepareFinalMessage(passwordDigest, responsePayload)
-  let requestContinue1 = %*{
-    "saslContinue": 1'i32,
-    "conversationId": toInt32(responseStart["conversationId"]),
-    "payload": bin(clientFinalMessage)
-  }
-  let responseContinue1 =  db["$cmd"].makeQuery(requestContinue1).one(ls)
+    clientFinalMessage =
+      scramClient.prepareFinalMessage(passwordDigest, responsePayload)
+    requestContinue1 = %*{
+      "saslContinue": 1'i32,
+      "conversationId": toInt32(responseStart["conversationId"]),
+      "payload": bin(clientFinalMessage)
+    }
+    responseContinue1 =  db["$cmd"].makeQuery(requestContinue1).one(ls)
 
   if responseContinue1["ok"].toFloat64() == 0.0:
     db.client.authenticated = false
     return false
 
-  if not scramClient.verifyServerFinalMessage(binstr(responseContinue1["payload"])):
-      raise newException(Exception, "Server returned an invalid signature.")
+  let payload = binstr(responseContinue1["payload"])
+  if not scramClient.verifyServerFinalMessage(payload):
+    raise Exception.newException "Server returned an invalid signature."
 
   # Depending on how it's configured, Cyrus SASL (which the server uses)
   # requires a third empty challenge.
-  if not responseContinue1["done"].toBool():
-      let requestContinue2 = %*{
-        "saslContinue": 1'i32,
-        "conversationId": responseContinue1["conversationId"],
-        "payload": ""
-      }
-      let responseContinue2 = db["$cmd"].makeQuery(requestContinue2).one(ls)
-      if not responseContinue2["done"].toBool():
-          raise newException(Exception, "SASL conversation failed to complete.")
+  if not responseContinue1["done"].toBool:
+    let requestContinue2 = %*{
+      "saslContinue": 1'i32,
+      "conversationId": responseContinue1["conversationId"],
+      "payload": ""
+    }
+    let responseContinue2 = db["$cmd"].makeQuery(requestContinue2).one(ls)
+    if not responseContinue2["done"].toBool:
+      raise Exception.newException "SASL conversation failed to complete."
   return true
